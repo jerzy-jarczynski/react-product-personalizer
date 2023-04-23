74ddab8b095dabce7aa9890a36d409b60ee0f598
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-04-23 22:32:20 +0200

    Change products to use array.map while rendering Product items. Add PropTypes to Product.

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,30 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const Product = props => {
+const Product = ({ name, title, colors, sizes, basePrice }) => {
+
+  console.log(name);
+  console.log(colors);
+  console.log(sizes);
+
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={ `${ title }` }
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${ name }--${ currentColor }.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{ title }</h2>
+          <span className={styles.price}>Price: { basePrice }$</span>
         </header>
         <form>
           <div className={styles.sizes}>
@@ -41,6 +51,14 @@ const Product = props => {
       </div>
     </article>
   )
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  colors: PropTypes.array.isRequired,
+  sizes: PropTypes.array.isRequired,
+  basePrice: PropTypes.number.isRequired
 };
 
 export default Product;
